26b9d5dfdc7fa5d1bb203fbdb083590db202a0bd
wegoedod <weedgoods42@gmail.com>
2023-12-06 19:18:33 +0900

    10x10 픽셀의 밝기를 평균내서 출력하는 기능 구현

--- Sources/ProjectAscii/ProjectAscii/main.c
+++ Sources/ProjectAscii/ProjectAscii/main.c
@@ -15,15 +15,28 @@ void changeToAscii(unsigned char* image, int width, int height, struct ImageInfo
     int gray;
     int index;
 
-    for (int i = height; i > 0; i--) {
-        for (int j = 0; j < width; j++) {
-            index = (i * width + j) * 3;
-            // RGB 값을 흑백으로 변환
-            gray = (int)(0.2989 * image[index] + 0.5870 * image[index + 1] + 0.1140 * image[index + 2]);
+    for (int i = height; i > 0; i-=10) {
+        for (int j = 0; j < width; j+=10) {
+            int sum = 0;
+            //밝기 합 구함
+            for (int k = 0; k < 10; k++) {
+                for (int l = 0; l < 10; l++) {
+                    int row = i + k;
+                    int col = j + l;
+                    if (row < height && col < width) {
+                        index = (row * width + col) * 3;
+                                // RGB 값을 흑백으로 변환
+                        sum += (int)(0.2989 * image[index] + 0.5870 * image[index + 1] + 0.1140 * image[index + 2]);
+                    }
+                }
+            }
 
+            //평균 밝기 계산
+            int average = sum / 100;
             // 밝기에 따라 아스키 문자 선택하여 출력
-            int char_index = (gray * (sizeof(info->ascii_chars) - 1)) / 255;
+            int char_index = (average * (sizeof(info->ascii_chars) - 1)) / 255;
             printf("%c", info->ascii_chars[char_index]);
+
         }
         printf("\n");
     }
