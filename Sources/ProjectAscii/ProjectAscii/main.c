416c2223ac0c5ff2dbfebe4a71cffa10a16ff9a3
wegoedod <weedgoods42@gmail.com>
2023-12-06 17:49:50 +0900

    구조체 추가

--- Sources/ProjectAscii/ProjectAscii/main.c
+++ Sources/ProjectAscii/ProjectAscii/main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <jpeglib.h>
 
 // 아스키 문자 세트
-char ascii_chars[] = "@%#*+=-:. ";
+struct ImageInfo{
+    char *ascii_chars;
+    char *filename;
+}IMAGEINFO;
+
 
 // 이미지를 아스키 아트로 변환하는 함수
-void changeToAscii(unsigned char* image, int width, int height) {
+void changeToAscii(unsigned char* image, int width, int height, struct ImageInfo *info) {
     int gray;
     int index;
 
@@ -17,19 +22,25 @@ void changeToAscii(unsigned char* image, int width, int height) {
             gray = (int)(0.2989 * image[index] + 0.5870 * image[index + 1] + 0.1140 * image[index + 2]);
 
             // 밝기에 따라 아스키 문자 선택하여 출력
-            int char_index = (gray * (sizeof(ascii_chars) - 1)) / 255;
-            printf("%c", ascii_chars[char_index]);
+            int char_index = (gray * (sizeof(info->ascii_chars) - 1)) / 255;
+            printf("%c", info->ascii_chars[char_index]);
         }
         printf("\n");
     }
 }
 
+
+
 int main() {
     // JPEG 이미지 파일 경로
-    const char* filename = "image.jpg";
+
+    struct ImageInfo info;
+    info.ascii_chars = "@%#*+=-:. ";
+    info.filename = "image.jpg";
+
 
     // JPEG 파일을 읽기 모드로 열기
-    FILE* file = fopen(filename, "rb");
+    FILE* file = fopen(info.filename, "rb");
     if (!file) {
         printf("파일을 열 수 없습니다.");
         return 1;
@@ -64,7 +75,7 @@ int main() {
     }
 
     // 이미지 출력
-    changeToAscii(image, cinfo.output_width, cinfo.output_height);
+    changeToAscii(image, cinfo.output_width, cinfo.output_height,&info);
 
     // 메모리 해제 및 파일 닫기
     free(image);
